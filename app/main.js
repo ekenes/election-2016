94c552121923ee71352ef6a61e5f3dfa1e00960d
Kristian Ekenes <kekenes@esri.com>
2020-09-16 14:15:45 -0700

    place other votes on top

--- app/main.js
+++ app/main.js
@@ -2398,6 +2398,81 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                                         type: "CIMVectorMarker",
                                         enable: true,
                                         anchorPoint: { x: 0, y: 0 },
+                                        offsetX: 0,
+                                        offsetY: 10,
+                                        anchorPointUnits: "Relative",
+                                        primitiveName: "other-positive-votes",
+                                        frame: { xmin: 0.0, ymin: 0.0, xmax: 17.0, ymax: 17.0 },
+                                        markerGraphics: [
+                                            {
+                                                type: "CIMMarkerGraphic",
+                                                geometry: {
+                                                    rings: [
+                                                        [
+                                                            [8.5, 0.2],
+                                                            [7.06, 0.33],
+                                                            [5.66, 0.7],
+                                                            [4.35, 1.31],
+                                                            [3.16, 2.14],
+                                                            [2.14, 3.16],
+                                                            [1.31, 4.35],
+                                                            [0.7, 5.66],
+                                                            [0.33, 7.06],
+                                                            [0.2, 8.5],
+                                                            [0.33, 9.94],
+                                                            [0.7, 11.34],
+                                                            [1.31, 12.65],
+                                                            [2.14, 13.84],
+                                                            [3.16, 14.86],
+                                                            [4.35, 15.69],
+                                                            [5.66, 16.3],
+                                                            [7.06, 16.67],
+                                                            [8.5, 16.8],
+                                                            [9.94, 16.67],
+                                                            [11.34, 16.3],
+                                                            [12.65, 15.69],
+                                                            [13.84, 14.86],
+                                                            [14.86, 13.84],
+                                                            [15.69, 12.65],
+                                                            [16.3, 11.34],
+                                                            [16.67, 9.94],
+                                                            [16.8, 8.5],
+                                                            [16.67, 7.06],
+                                                            [16.3, 5.66],
+                                                            [15.69, 4.35],
+                                                            [14.86, 3.16],
+                                                            [13.84, 2.14],
+                                                            [12.65, 1.31],
+                                                            [11.34, 0.7],
+                                                            [9.94, 0.33],
+                                                            [8.5, 0.2]
+                                                        ]
+                                                    ]
+                                                },
+                                                symbol: {
+                                                    type: "CIMPolygonSymbol",
+                                                    symbolLayers: [
+                                                        {
+                                                            type: "CIMSolidFill",
+                                                            enable: true,
+                                                            color: oColorCIM,
+                                                        }, {
+                                                            type: "CIMSolidStroke",
+                                                            enable: true,
+                                                            color: [161, 148, 0, 255],
+                                                            width: 1
+                                                        }
+                                                    ]
+                                                }
+                                            }
+                                        ],
+                                        scaleSymbolsProportionally: true,
+                                        respectFrame: true,
+                                    },
+                                    {
+                                        type: "CIMVectorMarker",
+                                        enable: true,
+                                        anchorPoint: { x: 0, y: 0 },
                                         offsetX: -10,
                                         offsetY: 0,
                                         anchorPointUnits: "Relative",
@@ -2535,81 +2610,6 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                                                             type: "CIMSolidStroke",
                                                             enable: true,
                                                             color: [153, 54, 3, 255],
-                                                            width: 1
-                                                        }
-                                                    ]
-                                                }
-                                            }
-                                        ],
-                                        scaleSymbolsProportionally: true,
-                                        respectFrame: true,
-                                    },
-                                    {
-                                        type: "CIMVectorMarker",
-                                        enable: true,
-                                        anchorPoint: { x: 0, y: 0 },
-                                        offsetX: 0,
-                                        offsetY: 10,
-                                        anchorPointUnits: "Relative",
-                                        primitiveName: "other-positive-votes",
-                                        frame: { xmin: 0.0, ymin: 0.0, xmax: 17.0, ymax: 17.0 },
-                                        markerGraphics: [
-                                            {
-                                                type: "CIMMarkerGraphic",
-                                                geometry: {
-                                                    rings: [
-                                                        [
-                                                            [8.5, 0.2],
-                                                            [7.06, 0.33],
-                                                            [5.66, 0.7],
-                                                            [4.35, 1.31],
-                                                            [3.16, 2.14],
-                                                            [2.14, 3.16],
-                                                            [1.31, 4.35],
-                                                            [0.7, 5.66],
-                                                            [0.33, 7.06],
-                                                            [0.2, 8.5],
-                                                            [0.33, 9.94],
-                                                            [0.7, 11.34],
-                                                            [1.31, 12.65],
-                                                            [2.14, 13.84],
-                                                            [3.16, 14.86],
-                                                            [4.35, 15.69],
-                                                            [5.66, 16.3],
-                                                            [7.06, 16.67],
-                                                            [8.5, 16.8],
-                                                            [9.94, 16.67],
-                                                            [11.34, 16.3],
-                                                            [12.65, 15.69],
-                                                            [13.84, 14.86],
-                                                            [14.86, 13.84],
-                                                            [15.69, 12.65],
-                                                            [16.3, 11.34],
-                                                            [16.67, 9.94],
-                                                            [16.8, 8.5],
-                                                            [16.67, 7.06],
-                                                            [16.3, 5.66],
-                                                            [15.69, 4.35],
-                                                            [14.86, 3.16],
-                                                            [13.84, 2.14],
-                                                            [12.65, 1.31],
-                                                            [11.34, 0.7],
-                                                            [9.94, 0.33],
-                                                            [8.5, 0.2]
-                                                        ]
-                                                    ]
-                                                },
-                                                symbol: {
-                                                    type: "CIMPolygonSymbol",
-                                                    symbolLayers: [
-                                                        {
-                                                            type: "CIMSolidFill",
-                                                            enable: true,
-                                                            color: oColorCIM,
-                                                        }, {
-                                                            type: "CIMSolidStroke",
-                                                            enable: true,
-                                                            color: [161, 148, 0, 255],
                                                             width: 1
                                                         }
                                                     ]
